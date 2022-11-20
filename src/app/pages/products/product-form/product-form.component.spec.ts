63201889860ece893eb2872e40d3a13f729cc0bc
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2022-11-20 15:51:17 +0100

    test: Added product-form test

--- src/app/pages/products/product-form/product-form.component.spec.ts
+++ src/app/pages/products/product-form/product-form.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProductFormComponent } from './product-form.component';
+import { ProductsService } from '../../../shared/services/products/products.service';
+import { MaterialModule } from '../../../shared/material/material.module';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateService, TranslateLoader, TranslateFakeLoader } from '@ngx-translate/core';
 
-describe('ProductFormComponent', () => {
+describe('(1) TEST - ProductFormComponent', () => {
   let component: ProductFormComponent;
   let fixture: ComponentFixture<ProductFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductFormComponent ]
+      declarations: [ ProductFormComponent ],
+      imports: [
+        MaterialModule,
+        HttpClientTestingModule, 
+        NoopAnimationsModule,
+        TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: TranslateFakeLoader
+          }
+        })],
+      providers:[ProductsService,
+        TranslateService,
+        [
+          {provide: MatDialogRef, useValue: {}},
+          {provide: MAT_DIALOG_DATA, useValue: []},
+      ]]
     })
     .compileComponents();
 
@@ -20,4 +42,21 @@ describe('ProductFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should return invalid form', () =>{
+    const form = component.productForm;
+    const name = component.nameControl;
+    name.setValue('Tornillo');
+    expect(form?.invalid).toBeTruthy();
+  });
+
+  it('Should return valid form', () =>{
+    const form = component.productForm;
+    component.nameControl.setValue('Tornillo');
+    component.priceControl.setValue(0.1);
+    component.formatControl.setValue(component.formats[0]);
+    component.markControl.setValue('Lorem');
+    expect(form?.valid).toBeTruthy();
+  });
+
 });
